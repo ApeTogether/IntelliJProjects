d0e15d5b5703c65cac0b791cc9c267f0e5d68ccb
Chris <lilunlogic@163.com>
2018-07-14 18:51:25 +0800

    add new files

---
+++ src/main/com/chris/demo/Demo1.java
@@ -0,0 +1,14 @@
+package main.com.chris.demo;
+
+/**
+ * @Auther: Chris Lee
+ * @Date: 7/14/2018 18:49
+ * @Description:
+ *
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("first demo");
+    }
+}
