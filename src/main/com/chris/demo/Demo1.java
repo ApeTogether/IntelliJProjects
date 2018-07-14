ae7dd998b042fe0792bca7484a742cff0d716803
chris <lilunlogic@163.com>
2018-07-14 19:18:10 +0800

    add sayGoodBye function

--- src/main/com/chris/demo/Demo1.java
+++ src/main/com/chris/demo/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 
     public static void main(String[] args) {
         sayHello("Chris", "how are you !");
+        sayGoodBye("Suan", "women");
     }
 
 
@@ -12,4 +13,15 @@ public class Demo1 {
         System.out.println("Hello : " + userName + ", " + greeting);
 
     }
+
+    public static void sayGoodBye(String userName, String gender) {
+        if ("man".equalsIgnoreCase(gender)) {
+            System.out.println("Goodbye, Mt " + userName);
+        } else if ("women".equalsIgnoreCase(gender)) {
+            System.out.println("Goodbye, Mr " + userName);
+        }
+
+    }
+
+
 }
