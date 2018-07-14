d48a6d0e733c2af160f2efecb2d2e19a88d48d34
Chris <lilunlogic@163.com>
2018-07-14 19:12:02 +0800

    expand sayHello function expand sayHello.jsp

--- src/main/com/chris/demo/Demo1.java
+++ src/main/com/chris/demo/Demo1.java
@@ -4,12 +4,12 @@ package main.com.chris.demo;
 public class Demo1 {
 
     public static void main(String[] args) {
-        sayHello("Chris");
+        sayHello("Chris", "how are you !");
     }
 
 
-    private static void sayHello(String userName) {
-        System.out.println("Hello : " + userName);
+    private static void sayHello(String userName, String greeting) {
+        System.out.println("Hello : " + userName + ", " + greeting);
 
     }
 }
--- src/main/webapp/sayhello.jsp
+++ src/main/webapp/sayhello.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World! I'm chris, I'm from CN</h2>
+<h2>Hello World! I'm chris, I'm from CN,  how are you?</h2>
 </body>
 </html>
