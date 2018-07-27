094dacbbd9eec3954b66c930db5f10dffbf59dc6
Chris <lilunlogic@163.com>
2018-07-27 18:37:19 +0800

    spring mvc 2018-07-27 18:32:01

--- src/main/com/chris/entity/Spittle.java
+++ src/main/com/chris/entity/Spittle.java
@@ -1,9 +1,41 @@
 package com.chris.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.Date;
+
 /**
  * @Auther Chris Lee
  * @Date 7/26/2018 14:00
  * @Description
  */
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Spittle {
+	
+	private final Long id = null;
+	
+	private final String message = null;
+	
+	private final Date time = null;
+	
+	private Double latitude;
+	
+	private Double longitude;
+	
+	public boolean equals(Object that) {
+		return EqualsBuilder.reflectionEquals(this, that, "id", "time");
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this, "id", "time");
+	}
 }
--- src/main/com/chris/repository/SpitterRepository.java
+++ src/main/com/chris/repository/SpitterRepository.java
@@ -1,6 +1,9 @@
 package com.chris.repository;
 
 import com.chris.entity.Spitter;
+import com.chris.entity.Spittle;
+
+import java.util.List;
 
 /**
  * @Auther Chris Lee
@@ -8,6 +11,9 @@ import com.chris.entity.Spitter;
  * @Description
  */
 public interface SpitterRepository {
+	
+	List<Spittle> findSpittles(int max, int count);
+	
 	Spitter save(Spitter spitter);
 	
 	Spitter findByUsername(String username);
